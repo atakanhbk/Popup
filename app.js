8f3a1a7c85ec33cf150bcdc11f77b25bf815c7d8
atakanhbk <atakanhbk@gmail.com>
2023-10-20 12:49:00 +0300

    Fix the code for browser console

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-function CreatePopUp(appendParent) {
-  const html = `<div class ="popUp">This is pop-up</div>`;
-  const style = "width : 100%; height:100%; background-color:red; z-index:100" 
+const popupContainer = document.createElement("div");
+popupContainer.innerHTML = `
+    <div class="popup-container">
+        <div class="popup-background">
+            <div class="popup-card">This place is Popup</div>
+        </div>
+    </div>
+`;
 
-  const popUpElement = document.createElement("div");
-  popUpElement.innerHTML = html;
-  popUpElement.style.cssText = style;
+const style = `
+    * {
+        margin: 0;
+        padding: 0;
+    }
 
-  appendParent.appendChild(popUpElement);
-  
-}
+    .popup-container, .popup-background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 9999999;
+    }
 
-CreatePopUp();
+    .popup-background {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
+
+    .popup-card {
+        position: absolute;
+        width: 50%;
+        height: 40%;
+        background-color: white;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+// Create a <style> element and set its content
+const styleElement = document.createElement("style");
+styleElement.innerHTML = style;
+
+// Append the style to the <head>
+document.head.appendChild(styleElement);
+
+// Append the popup container to the document body
+document.body.appendChild(popupContainer);
